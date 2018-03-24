65d847748855f55bb11b1e4724dc118204466e59
Tri Tran <tritran20@gmail.com>
2018-03-24 14:05:02 -0700

    Added days 7 and 8

---
+++ src/Days_Of_Code/Day8_DictionariesAndMaps.java
@@ -0,0 +1,23 @@
+package Days_Of_Code;
+
+import java.util.*;
+import java.io.*;
+
+public class Day8_DictionariesAndMaps {
+
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+        }
+        in.close();
+    }
+
+}
