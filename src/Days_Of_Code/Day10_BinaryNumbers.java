3743f1781fc26fa9f59c64616db4ba6d843d6437
Tri Tran <tritran20@gmail.com>
2018-03-26 21:34:09 -0700

    Finished day 10.

--- src/Days_Of_Code/Day10_BinaryNumbers.java
+++ src/Days_Of_Code/Day10_BinaryNumbers.java
@@ -28,15 +28,24 @@ public class Day10_BinaryNumbers {
 
         Scanner in = new Scanner(System.in);
         int baseTen = in.nextInt();
-        int binary[] = new int[100];
-        int index = 0;
+//        int binary[] = new int[100];
+        int count = 0, consecutive = 0;
 
         while (baseTen > 0) {
-            binary[index++] = baseTen%2;
-            baseTen = baseTen/2;
+//            binary[index++] = baseTen%2;
+//            baseTen = baseTen/2;
+            int remainder = baseTen%2;
+            if (remainder ==1){
+                consecutive++;
+                baseTen = baseTen/2;
+            } else {
+                consecutive = 0;
+                baseTen = baseTen/2;
+                consecutive = Math.max(consecutive, count);
+            }
         }
 
-        System.out.println(binary[index-1]);
+        System.out.println(consecutive);
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
 }
