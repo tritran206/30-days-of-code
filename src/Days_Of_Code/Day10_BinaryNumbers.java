5a6601b87cc1defdce3f6ddb1a1e2e50b3be95d3
Tri Tran <tritran20@gmail.com>
2018-03-26 19:23:11 -0700

    Added first part of day 10

---
+++ src/Days_Of_Code/Day10_BinaryNumbers.java
@@ -0,0 +1,42 @@
+package Days_Of_Code;
+
+import java.util.Scanner;
+
+public class Day10_BinaryNumbers {
+
+//    public static int[] ToBinary (int n) {
+
+//        int binary[] = new int[100];
+//        int index = 0;
+//
+//        while (n > 0) {
+//            binary[index++] = n%2;
+//            n = n/2;
+//        }
+//        return binary;
+//    }
+
+//    public static void PrintBinary(int[] b){
+//        int size = b.length;
+//
+//        for (int j = size; j >= 0; j--){
+//            System.out.println(b[j]);
+//        }
+//    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int baseTen = in.nextInt();
+        int binary[] = new int[100];
+        int index = 0;
+
+        while (baseTen > 0) {
+            binary[index++] = baseTen%2;
+            baseTen = baseTen/2;
+        }
+
+        System.out.println(binary[index-1]);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
